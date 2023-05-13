0689e416ac44b403816b9f6d87ec021df9713056
AndelaDujmov <andeladujmov3@gmail.com>
2023-05-13 21:45:16 +0200

    fixed issues

--- HygieiaApp/Areas/Admin/Controllers/ImmunizationController.cs
+++ HygieiaApp/Areas/Admin/Controllers/ImmunizationController.cs
@@ -1,3 +1,4 @@
+using HygieiaApp.DataAccess.Repositories;
 using HygieiaApp.DataAccess.Repositories.Impl;
 using HygieiaApp.Models.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +9,20 @@ namespace HygieiaApp.Areas.Admin;
 [Area(("Admin"))]
 public class ImmunizationController : Controller
 {
-    private readonly UnitOfWork _unitOfWork;
+    private readonly IUnitOfWork _unitOfWork;
     public List<string> ImmunizationType { get; set; } = new List<string>();
     
-    public ImmunizationController(UnitOfWork unitOfWork)
+    public ImmunizationController(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
     }
 
+    public IActionResult Index()
+    {
+        var immunizations = _unitOfWork.VaccineRepository.GetAll();
+        return View(immunizations);
+    }
+    
     public IActionResult Create()
     {
         return View();
@@ -38,6 +45,51 @@ public class ImmunizationController : Controller
             catch (Exception e)
             {
                 TempData["error"] = "Unable to add due to error!";
+                return View(e.Message);
+            }
+        }
+
+        return View(immunization);
+    }
+    
+    public IActionResult Edit(Guid? id)
+    {
+        if (id is null)
+        {
+            TempData["error"] = "Unable to edit immunization due to unknown id.";
+            return NotFound();
+        }
+
+        var medicalcond = _unitOfWork.VaccineRepository.Get(x=> x.Id == id);
+
+        if (medicalcond is null)
+        {
+            TempData["error"] = "Unable to update empty data.";
+            return NotFound();
+        }
+            
+        
+        return View(medicalcond);
+    }
+    
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Immunization immunization)
+    {
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _unitOfWork.VaccineRepository.Update(immunization);
+                _unitOfWork.Save();
+                TempData["success"] = "Immunization edited succesfully.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = "Unable to edit immunization due to error.";
                 return View(e.Message);
             }
         }
